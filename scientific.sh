121b5388469e6f9f2d824fed75d822b05ad91414
Trung Huynh <thuynh@palantir.com>
2014-12-30 23:12:39 +0000

    Updated to support Ubuntu instead of Fedora - Tested on Ubuntu 14.04 LTS

--- scientific.sh
+++ scientific.sh
@@ -4,10 +4,10 @@
 # Copyright (C) 2014 trunghuynh <trunghuynh@Trungs-MBP.home>
 #
 echo "Installing git..."
-sudo yum install git
+sudo apt-get install git
 
 echo "Install tmux.."
-sudo yum install tmux
+sudo apt-get install tmux
 
 echo "Configuring dot files..."
 git clone https://github.com/trunghlt/dotfiles.git
@@ -21,10 +21,12 @@ echo "Installing vim spf..."
 curl http://j.mp/spf13-vim3 -L -o - | sh
 
 echo "Installing python virtual environment..."
-sudo yum install python-devel python-pip
+sudo apt-get install python-dev
+sudo easy_install pip
 sudo pip install virtualenv
-virtualenv trung
+virtualenv scientific
 echo "source trung/bin/activate" >> ~/.bashrc
-source trung/bin/activate
-sudo yum install lapack lapack-devel blas blas-devel
-pip install numpy scipy theano pandas ipython jinja2 pyzmq
+source scientific/bin/activate
+sudo apt-get install gcc gfortran libblas-dev liblapack-dev
+pip install numpy scipy
+pip install theano pandas ipython jinja2 pyzmq tornado
